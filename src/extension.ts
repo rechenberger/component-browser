ad5d9cc7029dea24e9340ca7126bb1e9b9a1a095
Tristan <tristan@Tristans-MacBook-Pro.local>
2017-06-26 18:11:40 +0200

    parsePath

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,18 @@
 'use strict';
 import * as vscode from 'vscode';
-import { init } from "./init";
+import { ComponentBrowser } from "./init";
 
 export function activate(context: vscode.ExtensionContext) {
 
-  console.log('Congratulations, your extension "component-browser" is now active!');
+  new ComponentBrowser()
+
+  console.log('Congratulations, your extension "component-browser" is now active!')
 
   let disposable = vscode.commands.registerCommand('extension.cb.init', () => {
-    init()
-  });
+    // Does Nothing
+  })
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable)
 }
 
 export function deactivate() {
--- src/init.ts
+++ src/init.ts
@@ -1,15 +1,45 @@
 'use strict';
 import * as vscode from 'vscode';
+import * as _ from 'lodash'
+import * as nodepath from "path";
 
+const ending = `.component.ts`
+const searchString = `**/*${ending}`
+const excludeString = `**/node_modules/**`
+const selectorPrefix = `app-`
 
-export function init() {
-  const searchString = "**/*.component.ts"
-  const excludeString = "**/node_modules/**"
+export class ComponentBrowser {
 
-  vscode.workspace.findFiles(searchString, excludeString)
-    .then(files => files.map(f => f.path))
-    .then(console.log)
+  components = []
 
-  
+  constructor() {
+    this.initComponents();
+  }
+
+  initComponents() {
+    this.getAllFilePaths()
+      .then(paths => _.map(paths, p => this.parsePath(p)))
+      .then(components => this.components = components)
+      .then(data => console.log(data))
+
     vscode.window.showInformationMessage('Done');
-}
+  }
+
+  getAllFilePaths() {
+    return vscode.workspace.findFiles(searchString, excludeString)
+      .then(files => files.map(f => f.path))
+  }
+
+  parsePath(path) {
+    const p = nodepath.parse(path)
+    const name = p.base.replace(ending, '')
+    const selector = selectorPrefix + name;
+    return {
+      path,
+      name,
+      selector
+    };
+  }
+
+
+} 
