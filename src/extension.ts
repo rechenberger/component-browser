0ef27a647f11d75adedcbcf260d27400d1ec513e
Tristan <rechenberger@me.com>
2017-06-27 14:12:43 +0200

    cdp

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,15 @@
 'use strict';
 import * as vscode from 'vscode';
 import { ComponentBrowser } from "./component-browser";
+import { startCDP } from "./cdp";
 
 export function activate(context: vscode.ExtensionContext) {
 
   const cb = new ComponentBrowser()
 
   let disposable = vscode.commands.registerCommand('extension.cb.open', () => {
-    cb.open();
+    cb.open()
+    startCDP()
   })
 
   context.subscriptions.push(disposable)
